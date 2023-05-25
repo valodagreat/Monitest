e5a6d6a017ef17c9fd4b0b9f32346bd6a037fba3
Valentine Ifedayo <vcaleb01@gmail.com>
2023-05-25 09:28:25 +0100

    FEAT: SETUP PROJECT STRUCTURE AND DATABASE SCHEMAS

---
+++ src/models/user.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match : [
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please input a valid email']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength : 6,
+    },
+}, {
+    timestamps: true
+})
+
+const User = mongoose.model('User',userSchema);
---
+++ src/models/wallet.ts
@@ -0,0 +1,5 @@
+import mongoose from 'mongoose';
+
+const walletSchema = new mongoose.Schema({
+    
+})
